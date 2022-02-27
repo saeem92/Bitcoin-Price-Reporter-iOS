2bc09c66d66f44186ba5a023f2bb42faf93ce466
saeem92 <93969890+saeem92@users.noreply.github.com>
2022-02-28 01:33:16 +0530

    Initial Commit

---
+++ ByteCoin/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by Saeem on 28/02/2022.
+//  Copyright © 2022 Saeem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,19 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by Saeem on 28/02/2022.
+//  Copyright © 2022 Saeem. All rights reserved.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "API"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+    
+}
